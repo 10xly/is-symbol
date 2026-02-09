10a2ddc267df54b4824db51414f700522a8b61c7
tj-commits <thefoxjones@gmail.com>
2026-02-09 16:40:19 -0600

    refactor: add ts definitions and change code some

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var TYPE = require('@extremejs/utils').TYPE
 var hasSymbols = require('has-symbols')()
+var type = require("es-typeof")
 
 if (hasSymbols) {
   module.exports = function isSymbol(value) {
-    return typeof value === TYPE.SYMBOL
+    return type(value) === TYPE.SYMBOL
   }
 } else {
-  module.exports = function isSymbol(value) {
-    return false && value
-  }
+  module.exports =require("false-value")
 }
